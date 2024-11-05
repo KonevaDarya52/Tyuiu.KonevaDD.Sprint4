e8fc3e50a90a94d0f8e0b3c9577091e88ee7ee27
Дарья Конева <Tyuiu.KonevaDD@outlook.com>
2024-11-05 17:43:09 +0500

    Task0

---
+++ Tyuiu.KonevaDD.Sprint4.Task0.V17.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.KonevaDD.Sprint4.Task0.V17.Lib
+{
+    public class DataService : ISprint4Task0V17
+    {
+        public int GetSumEvenArrEl(int[] array)
+        {
+            int sum = 0;
+            foreach (int num in array)
+            {
+                if (num % 2 == 0) 
+                {
+                    sum += num;
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ Tyuiu.KonevaDD.Sprint4.Task0.V17/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.KonevaDD.Sprint4.Task0.V17.Lib;
+namespace Tyuiu.KonevaDD.Sprint4.Task0.V17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int[] numsArray = { 4, 8, 7, 6, 5, 8, 2, 4, 3, 2 };
+            Console.WriteLine("Исходный массив: ");
+            foreach (int num in numsArray)
+            {
+                Console.Write(num + " ");
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int result = ds.GetSumEvenArrEl(numsArray);
+            Console.WriteLine("Сумма четных элементов массива: " + result);
+            Console.ReadKey();
+
+
+
+        }
+       
+
+
+    }
+
+
+
+
+
+}
